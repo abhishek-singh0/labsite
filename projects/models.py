6d6be2eb2f6381daf5b9d1318f550125065e45b6
Pratyush <prbehere93@gmail.com>
2021-07-15 16:03:33 +0530

    made migrations, changed due_date field

--- projects/models.py
+++ projects/models.py
@@ -3,28 +3,33 @@ from django.contrib.auth.models import User
 from django.db.models.deletion import PROTECT
 from django_extensions.db.models import TimeStampedModel
 
-class Projects(TimeStampedModel):
+class Project(TimeStampedModel):
     title=models.CharField(max_length=100)
     desription=models.CharField(max_length=1000,blank=True)
-    pass
 
-class Assignments(TimeStampedModel):
+    def __str__(self):
+        return self.title
+    
+
+class Assignment(TimeStampedModel):
     class Status(models.TextChoices):
         COMPLETED="Completed"
         PENDING="Pending"   
         BACKLOG="Backlog"
 
     user=models.ForeignKey(User,on_delete=models.PROTECT)
-    project=models.ForeignKey(Projects,on_delete=models.PROTECT) #need to verify this
+    project=models.ForeignKey(Project,on_delete=models.PROTECT) #need to verify this
     title=models.CharField(max_length=50)
     description=models.CharField(max_length=1000,blank=True)
     status=models.CharField(max_length=20,default=Status.PENDING,choices=Status.choices)
-    due_date=models.DateField()
-    pass
+    due_date=models.DateField(blank=True, null=True)
 
-class comments(TimeStampedModel):
-    assignment=models.ForeignKey(Assignments,on_delete=models.PROTECT)
+    def __str__(self):
+        return self.title
+
+class Comment(TimeStampedModel):
+    assignment=models.ForeignKey(Assignment,on_delete=models.PROTECT)
     user=models.ForeignKey(User,on_delete=models.PROTECT)
-    body=models.CharField(max_length=150,blank=True)
-    pass
+    body=models.CharField(max_length=150,blank=True)            
+
 
